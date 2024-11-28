c946aed8a6489805d661c3d8441cf09f9a1f204d
Bruno Pajdek <brupaj@proton.me>
2024-11-28 17:22:38 +0100

    Comment in_docker.sh

--- in_docker.sh
+++ in_docker.sh
@@ -2,7 +2,11 @@
 # SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 # SPDX-FileCopyrightText: 2024 Harald Sitter <sitter@kde.org>
 
+# First thing that runs inside of Arch Linux Docker containers created by build-docker.sh.
+
+# Exit immediately if any command fails and print all commands before they are executed.
 set -ex
 
+# Bootstrap container for building and build.
 ./bootstrap.sh
 ./build.sh "$@"
